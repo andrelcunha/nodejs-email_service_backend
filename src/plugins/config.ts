6fe689da546e17e822d686e2a3ef0fa0f427ed8c
Andre Cunha <andreluiscunha81@gmail.com>
2024-02-27 05:56:51 -0300

    Added credencial keys to config file

--- src/plugins/config.ts
+++ src/plugins/config.ts
@@ -10,6 +10,8 @@ dotenv.config({
 export const config = {
   HOST: process.env.HOST,
   PORT: process.env.PORT,
+  AWS_ACCESS_KEY: process.env.ACCESS_KEY,
+  AWS_SECRET_KEY: process.env.SECRET_KEY,
   AWS_REGION: process.env.AWS_REGION,
   AWS_SES_SENDER: process.env.AWS_SES_SENDER,
 };
